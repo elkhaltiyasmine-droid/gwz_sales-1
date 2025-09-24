0a916fc53c8ab0fc615c52958895f13fc07be492
elkhaltiyasmine-droid <elkhaltiyasmine@gmail.com>
2025-09-24 14:12:31 +0100

    develop

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,4 +1,4 @@
-SELECT ROUND(SUM(turnover),2),date_date , ROUND(sum(purchase_cost),2)
+SELECT SUM(turnover),date_date , ROUND(sum(purchase_cost))
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
 GROUP BY date_date
 ORDER BY date_date
