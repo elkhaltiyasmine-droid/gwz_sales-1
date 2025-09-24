97b354b7561ef0b621afdebdba8a662e1b92e75c
elkhaltiyasmine-droid <elkhaltiyasmine@gmail.com>
2025-09-24 12:23:00 +0100

    add_purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,4 +1,4 @@
-SELECT SUM(turnover),date_date
+SELECT SUM(turnover),date_date , ROUND(sum(purchase_cost))
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
 GROUP BY date_date
 ORDER BY date_date
